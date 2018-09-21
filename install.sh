38130e5713fcfe63558f00f00c07f96e52f16feb
Jared Knutson <drednaut@DumpsterDiving.local>
2018-09-21 08:56:23 -0700

    install script

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Install Airline
 echo ****** Installing Airline ******
-git clone https://github.com/vim-airline/vim-airline.git ./.vim/bundle/
+git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/
 
 # Install Airline-themes
 echo ****** Installing Airline Themes ******
-git clone https://github.com/vim-airline/vim-airline-themes.git ./.vim/bundle/
+git clone https://github.com/vim-airline/vim-airline-themes.git ~/.vim/bundle/
 
 # Install Fugitive
 echo ****** Installing Fugitive ******
-git clone https://github.com/tpope/vim-fugitive.git ./.vim/bundle/
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/
 
 # Install Powerline Fonts
 echo ****** Installing Powerline Fonts ******
